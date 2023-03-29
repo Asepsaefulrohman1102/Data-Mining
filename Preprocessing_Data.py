9bbcb3ea683479d4dd64dcfa1f13f410aa876c19
Asepsaefulrohman11 <111202113705@mhs.dinus.ac.id>
2023-03-29 23:24:42 +0700

    updated

--- Preprocessing_Data.py
+++ Preprocessing_Data.py
@@ -8,15 +8,19 @@ y = dataset.iloc[:, -1].values
 
 print(x)
 print(y)
-# from sklearn.impute import SimpleImputer
-# imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-# imputer = imputer.fit(x[:, 1:3])
-# x[:, 1:3] = imputer.transform(x[:, 1:3])
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
 
-
+print(x)
+print(y)
 
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 x = np.array(ct.fit_transform(x))
+
+print(x)
+print(y)
 
